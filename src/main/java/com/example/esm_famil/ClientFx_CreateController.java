c0af5423619e1fe84ad567165b9e08ebfbc3d0aa
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-01 19:48:58 +0430

    some ids added

--- src/main/java/com/example/esm_famil/ClientFx_CreateController.java
+++ src/main/java/com/example/esm_famil/ClientFx_CreateController.java
@@ -1,4 +1,68 @@
 package com.example.esm_famil;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.controls.MFXToggleButton;
+import io.github.palexdev.materialfx.controls.legacy.MFXLegacyComboBox;
+import javafx.fxml.FXML;
+
 public class ClientFx_CreateController {
+
+    @FXML
+    private MFXCheckbox createPageFirstnameBox;
+
+    @FXML
+    private MFXCheckbox createPageFoodBox;
+
+    @FXML
+    private MFXCheckbox createPageLastnameBox;
+
+    @FXML
+    private MFXCheckbox createPageObjectBox;
+
+    @FXML
+    private MFXCheckbox createPageCityBox;
+
+    @FXML
+    private MFXCheckbox createPageFruitBox;
+
+    @FXML
+    private MFXCheckbox createPageCountryBox;
+
+    @FXML
+    private MFXCheckbox createPageCarBox;
+
+    @FXML
+    private MFXCheckbox createPageFlowerBox;
+
+    @FXML
+    private MFXCheckbox createPageAnimalBox;
+
+    @FXML
+    private MFXTextField createPagePlayerNameFld;
+
+    @FXML
+    private MFXButton createPageCreateBtn;
+
+    @FXML
+    private MFXToggleButton createPageType;
+
+    @FXML
+    private MFXTextField createPageGroupNameFld;
+
+    @FXML
+    private MFXPasswordField createPagePasswordFld;
+
+    @FXML
+    private MFXLegacyComboBox<String> createPageNumberOfRound;
+
+    @FXML
+    void initialize() {
+        createPageNumberOfRound.getItems().addAll("1", "2", "3", "4");
+        createPageNumberOfRound.getSelectionModel().select("1");
+
+
+    }
 }
