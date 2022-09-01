f5764c2d96907eb3ded7ba45893f60309c2a3e15
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-01 19:50:56 +0430

    some array lists like clientManagers and games added

--- src/main/java/com/example/esm_famil/model/Server.java
+++ src/main/java/com/example/esm_famil/model/Server.java
@@ -3,10 +3,19 @@ package com.example.esm_famil.model;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
+    private ArrayList<Game> games;
+    private ArrayList<ArrayList<ClientManager>> clientManagers = new ArrayList<>(5);
     private ServerSocket serverSocket;
     private int serverPort = 8080;
+
+    {
+        for (int i = 0; i < clientManagers.size(); i++) {
+            clientManagers.add(new ArrayList<ClientManager>());
+        }
+    }
 
     public Server () {
         try {
