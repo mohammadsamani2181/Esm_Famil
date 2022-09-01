7348a6d0a30d66390621d24141b9b9f48ba9de80
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-02 02:31:47 +0430

    a new class for Main_server created

---
+++ src/main/java/com/example/esm_famil/Main_Server.java
@@ -0,0 +1,10 @@
+package com.example.esm_famil;
+
+import com.example.esm_famil.model.Server;
+
+public class Main_Server {
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
