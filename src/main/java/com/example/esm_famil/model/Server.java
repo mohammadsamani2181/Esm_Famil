f670c4deb24620d5b65c892b7bf53f46c8858772
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-02 02:30:58 +0430

    createNewGame method for creating a new game added addGameFields method for adding game's fields added

--- src/main/java/com/example/esm_famil/model/Server.java
+++ src/main/java/com/example/esm_famil/model/Server.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
-    private ArrayList<Game> games;
+    private ArrayList<Game> games = new ArrayList<>();
     private ArrayList<ArrayList<ClientManager>> clientManagers = new ArrayList<>(5);
     private ServerSocket serverSocket;
     private int serverPort = 8080;
@@ -27,5 +27,19 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public int createNewGame (String password) {
+        Game newGame = new Game(password);
+        games.add(newGame);
+
+        // debug
+        System.out.println("in server gameId :" + newGame.getId());
+        System.out.println("in server gamePassword :" + newGame.getPassword());
+        return newGame.getId();
+    }
+
+    public void addGameFields(int gameId, String text) {
+        games.get(gameId).addField(text);
     }
 }
