62582361d3bce03277ebc6a9dd17f32230b462a2
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-02 02:16:14 +0430

    createGame method for connection to the server and creating a new game created and also sendingGameFields for sending game fields to the server created

--- src/main/java/com/example/esm_famil/model/Client.java
+++ src/main/java/com/example/esm_famil/model/Client.java
@@ -1,7 +1,10 @@
 package com.example.esm_famil.model;
 
+import com.example.esm_famil.ClientFx_CreateController;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
@@ -10,8 +13,12 @@ public class Client {
     private String serverAddress = "127.0.0.1";
     private BufferedReader reader;
     private PrintWriter writer;
+    private ClientFx_CreateController clientFxCreateController;
+    private int gameId;
 
-    public Client () {
+    public Client (ClientFx_CreateController clientFxCreateController) {
+        this.clientFxCreateController = clientFxCreateController;
+
         try {
             socket = new Socket(serverAddress, port);
 
@@ -25,8 +32,39 @@ public class Client {
                             new OutputStreamWriter(
                                     socket.getOutputStream())), true);
 
+            Thread thread = new Thread(new ServerMessageManager(reader, this));
+            thread.start();
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+
+    public int createGame (String password) {
+        // debug
+        System.out.println("in client create game");
+        writer.println("CREATE NEW GAME");
+        writer.println(password);
+
+        return gameId;
+    }
+
+    public void sendingGameFields (ArrayList<String> texts) {
+        // debug
+        System.out.println("in client sending fields");
+
+        writer.println("GAME FIELDS");
+        writer.println(gameId);
+        writer.println(texts.size());
+
+        for (int i = 0; i < texts.size(); i++) {
+            writer.println(texts.get(i));
         }
     }
 }
