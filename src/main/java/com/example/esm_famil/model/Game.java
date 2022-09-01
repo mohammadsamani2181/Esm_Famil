0eb806021c5a94de7f81c9c2ebd031999f3048f0
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-02 02:26:51 +0430

    some getters and setters for some fields added and a new observableList for game's fields created and two method for adding texts to the observableList added

--- src/main/java/com/example/esm_famil/model/Game.java
+++ src/main/java/com/example/esm_famil/model/Game.java
@@ -1,15 +1,45 @@
 package com.example.esm_famil.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
 public class Game {
-    private static int idNumber = -1;
+    private static int idNumber = 0;
     private int id;
     private String password;
-
-    {
-        id = idNumber++;
-    }
+    private ObservableList<String> fields = FXCollections.observableArrayList();
 
     public Game(String password) {
+        id = idNumber++;
         this.password = password;
+    }
+
+    public Game () {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addField (String text) {
+        fields.add(text);
+    }
+
+    public void addField (ArrayList<String> texts) {
+        fields.addAll(texts);
     }
 }
