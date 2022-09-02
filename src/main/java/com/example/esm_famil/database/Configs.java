b513bda5323820610f9723d3e542afb6a5519ae4
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-02 22:58:57 +0430

    database configs added

---
+++ src/main/java/com/example/esm_famil/database/Configs.java
@@ -0,0 +1,9 @@
+package com.example.esm_famil.database;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPassword = "@Mohammad0921";
+    protected String dbName = "esm_famil";
+}
