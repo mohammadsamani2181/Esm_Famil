6223b5a57a69f67f8bc3a5c2775004b0738ea4f9
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-02 22:59:57 +0430

    Const class created and some information about games table added

---
+++ src/main/java/com/example/esm_famil/database/Const.java
@@ -0,0 +1,14 @@
+package com.example.esm_famil.database;
+
+public class Const {
+    //Table name
+    public static final String GAMES_TABLE = "games";
+
+
+    //Games Table Column Names
+    public static final String GAMES_ID = "gameid";
+    public static final String GAMES_HOSTNAME = "hostname";
+    public static final String GAMES_GROUPNAME = "groupname";
+    public static final String GAMES_PASSWORD = "password";
+    public static final String GAMES_DATECREATED = "datecreated";
+}
