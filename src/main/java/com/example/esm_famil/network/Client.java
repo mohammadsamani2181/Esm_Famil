8ca7ea1c87e7ea7b279bf659e32cb5e1f38cb69a
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-04 19:13:35 +0430

    a new constructor added

--- src/main/java/com/example/esm_famil/network/Client.java
+++ src/main/java/com/example/esm_famil/network/Client.java
@@ -39,6 +39,30 @@ public class Client {
     }
 
 
+    public Client () {
+
+        try {
+            socket = new Socket(serverAddress, port);
+
+            reader =  new BufferedReader(
+                    new InputStreamReader(
+                            socket.getInputStream()));
+
+
+            writer = new PrintWriter(
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    socket.getOutputStream())), true);
+
+            Thread thread = new Thread(new ServerMessageManager(reader, clientFxCreateController));
+            thread.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     public void createGame (String password, String hostName, String groupName, String numberOfRound) {
         writer.println("CREATE NEW GAME");
