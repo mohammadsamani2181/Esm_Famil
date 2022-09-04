540cf9abf0e5f7b844f8f05404c00172d72b3e62
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-04 13:16:03 +0430

    loading way changed

--- src/main/java/com/example/esm_famil/Main_Client.java
+++ src/main/java/com/example/esm_famil/Main_Client.java
@@ -2,6 +2,7 @@ package com.example.esm_famil;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,11 @@ import java.io.IOException;
 public class Main_Client extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main_Client.class.getResource("clientFx_Create.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 650, 430);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main_Client.class.getResource("clientFx_Join.fxml"));
+        fxmlLoader.load();
+        Parent root = fxmlLoader.getRoot();
         stage.setTitle("Esm & Famil Game");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
