ea474ebb84ea165f43193b8fb1f3923111f18900
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-04 13:06:43 +0430

    some ids added and updateItem method created and now game information will add to the cell

--- src/main/java/com/example/esm_famil/CellController.java
+++ src/main/java/com/example/esm_famil/CellController.java
@@ -1,4 +1,66 @@
 package com.example.esm_famil;
 
-public class CellController {
+import com.example.esm_famil.model.Game;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class CellController extends ListCell<Game> {
+
+    private FXMLLoader loader;
+
+    @FXML
+    private AnchorPane cellPane;
+
+    @FXML
+    private Label cellDateCreated;
+
+    @FXML
+    private Label cellGroupName;
+
+    @FXML
+    private Label cellHostName;
+
+    @FXML
+    private Label cellNumberOfRound;
+
+    @FXML
+    private MFXButton cellJoinButton;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @Override
+    protected void updateItem(Game game, boolean empty) {
+        super.updateItem(game, empty);
+
+        if (game == null || empty) {
+            setText(null);
+            setGraphic(null);
+        }else {
+            loader = new FXMLLoader(getClass().getResource("cell.fxml"));
+            loader.setController(this);
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            cellHostName.setText(game.getHostName());
+            cellGroupName.setText(game.getGroupName());
+            cellNumberOfRound.setText(Integer.toString(game.getNumberOfRound()));
+            cellDateCreated.setText(game.getDateCreated().toString());
+
+            setText(null);
+            setGraphic(cellPane);
+        }
+    }
 }
