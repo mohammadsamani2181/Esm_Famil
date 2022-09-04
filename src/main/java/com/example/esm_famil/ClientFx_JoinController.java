d295d812413f2ecc8f2a2265915d79e80fc59cf8
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-04 13:12:05 +0430

    addGamesToTheGamesList method created for adding existing games in database to the listView and showCreatePage method created for showing clientFx_Create

--- src/main/java/com/example/esm_famil/ClientFx_JoinController.java
+++ src/main/java/com/example/esm_famil/ClientFx_JoinController.java
@@ -1,14 +1,87 @@
 package com.example.esm_famil;
 
+import com.example.esm_famil.database.Const;
+import com.example.esm_famil.database.DBHandler;
+import com.example.esm_famil.model.Game;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.legacy.MFXLegacyListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class ClientFx_JoinController {
-    @FXML
-    private Label welcomeText;
+
+    private ObservableList games = FXCollections.observableArrayList();
+
+    private DBHandler dbHandler;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MFXLegacyListView<Game> joinPageListView;
+
+    @FXML
+    private MFXButton joinPageCreateNewGameBtn;
+
+    @FXML
+    void initialize() {
+        addGamesToTheGamesList();
+
+        joinPageCreateNewGameBtn.setOnAction(e -> {
+            showCreatePage();
+        });
+    }
+
+    private void addGamesToTheGamesList(){
+        dbHandler = new DBHandler();
+        ResultSet resultSet = dbHandler.getGames();
+
+        try {
+            while (resultSet.next()) {
+                Game game = new Game();
+
+                String hostname = resultSet.getString(Const.GAMES_HOSTNAME);
+                String groupName = resultSet.getString(Const.GAMES_GROUPNAME);
+                Timestamp dateCreated = resultSet.getTimestamp(Const.GAMES_DATECREATED);
+                int numberOfRound = resultSet.getInt(Const.GAMES_NUMBEROFROUND);
+
+                game.setHostName(hostname);
+                game.setGroupName(groupName);
+                game.setDateCreated(dateCreated);
+                game.setNumberOfRound(numberOfRound);
+
+                games.add(game);
+            }
+
+            joinPageListView.setItems(games);
+            joinPageListView.setCellFactory(CellController -> new CellController());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showCreatePage() {
+        joinPageCreateNewGameBtn.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("clientFx_Create.fxml"));
+
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        Parent root = loader.getRoot();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
