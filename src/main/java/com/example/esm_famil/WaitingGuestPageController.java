ca393545df9393300df2309d97ad13852f8b09a7
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-06 22:10:05 +0430

    created and some ids added

---
+++ src/main/java/com/example/esm_famil/WaitingGuestPageController.java
@@ -0,0 +1,15 @@
+package com.example.esm_famil;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class WaitingGuestPageController {
+
+    @FXML
+    private AnchorPane waitingGuestPagePane;
+
+    @FXML
+    void initialize() {
+
+    }
+}
