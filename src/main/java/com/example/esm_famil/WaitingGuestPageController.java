e1b8435eb33737256a14c6ee84e371ee6ca46512
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-19 18:13:33 +0430

    three new field (game, client, message Manager) and their getters and setters added

--- src/main/java/com/example/esm_famil/WaitingGuestPageController.java
+++ src/main/java/com/example/esm_famil/WaitingGuestPageController.java
@@ -1,9 +1,19 @@
 package com.example.esm_famil;
 
+import com.example.esm_famil.model.Game;
+import com.example.esm_famil.network.Client;
+import com.example.esm_famil.network.ServerMessageManagerJoiningGame;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
 public class WaitingGuestPageController {
+
+    private Game game;
+
+    private Client client;
+
+    private ServerMessageManagerJoiningGame messageManager;
 
     @FXML
     private AnchorPane waitingGuestPagePane;
