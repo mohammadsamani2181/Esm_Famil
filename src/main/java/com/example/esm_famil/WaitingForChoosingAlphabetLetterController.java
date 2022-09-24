60a8606238bec866e5fcc572917a265134128460
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-24 21:37:45 +0330

    created and an id for main pane added

---
+++ src/main/java/com/example/esm_famil/WaitingForChoosingAlphabetLetterController.java
@@ -0,0 +1,15 @@
+package com.example.esm_famil;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class WaitingForChoosingAlphabetLetterController {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    void initialize() {
+
+    }
+}
