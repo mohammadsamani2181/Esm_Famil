8eea0741e1bbd053fbe1217642f8b22c4f187861
mmd_s21 <hdhdmohammad5@gmail.com>
2022-09-24 21:21:46 +0330

    created and some ids added and a limitation for Text Field added so if you enter a letter and click on start Btn, this letter will send to the server and then gamePage will show

---
+++ src/main/java/com/example/esm_famil/ChooseAlphabetLetterController.java
@@ -0,0 +1,80 @@
+package com.example.esm_famil;
+
+import com.example.esm_famil.model.Game;
+import com.example.esm_famil.network.Client;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
+public class ChooseAlphabetLetterController {
+
+    private Client client;
+
+    private Game game;
+
+    private StartGame startGame;
+
+    @FXML
+    private MFXTextField chooseAlphabetTxtFld;
+
+    @FXML
+    private MFXButton chooseAlphabetStartBtn;
+
+    @FXML
+    void initialize() {
+        chooseAlphabetTxtFld.setTextLimit(1);
+
+        chooseAlphabetStartBtn.setOnAction(e -> {
+            sendAlphabetLetter();
+        });
+    }
+
+    private void sendAlphabetLetter() {
+        if (!chooseAlphabetTxtFld.getText().equals("")) {
+            client.sendAlphabetLetter(game.getId(), chooseAlphabetTxtFld.getText());
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            showGamePage();
+
+        }else {
+            System.out.println("you must enter something");
+        }
+    }
+
+    private void showGamePage() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamePage.fxml"));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        GamePageController controller = loader.getController();
+        controller.setShowGamePage(startGame);
+        controller.showCells();
+
+        startGame.getPane().getChildren().setAll((Node) loader.getRoot());
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void setShoGamePage(StartGame startGame) {
+        this.startGame = startGame;
+    }
+}
+
